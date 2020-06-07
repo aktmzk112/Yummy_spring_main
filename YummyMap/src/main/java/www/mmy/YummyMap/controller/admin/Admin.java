08280084b4f9593c70da10c0ddb506d68f2f2b31
aktmzk112 <aktmzk112@gmail.com>
2020-06-07 15:59:00 +0900

    commit

--- YummyMap/src/main/java/www/mmy/YummyMap/controller/admin/Admin.java
+++ YummyMap/src/main/java/www/mmy/YummyMap/controller/admin/Admin.java
@@ -1,5 +1,7 @@
 package www.mmy.YummyMap.controller.admin;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import www.mmy.YummyMap.dao.AdminDAO;
+import www.mmy.YummyMap.util.PageUtil;
 import www.mmy.YummyMap.vo.AdminVO;
 
 @Controller
@@ -25,18 +28,29 @@ public class Admin {
 	
 	//관리자 로그인 전담 함수
 	@RequestMapping("/loginProc.mmy")
-	public ModelAndView loginck(AdminVO avo , ModelAndView mv) {
+	public ModelAndView loginck(AdminVO avo , ModelAndView mv , HttpSession session) {
 		int cnt = adminDao.loginck(avo);
 		System.out.println(cnt + " cnt ##################################");
 		RedirectView rv =null;
 		if(cnt == 1) {
-			System.out.println("정상 로그인");
-			 rv = new RedirectView("/YummyMap/admin/login.mmy");
+			 session.setAttribute("SID", avo.getMid());
+			 rv = new RedirectView("/YummyMap/admin/main.mmy");
 		}else {
 			 rv = new RedirectView("/YummyMap/admin/login.mmy?noad=b");
 		}
 		mv.setView(rv);
 		return mv;
 	}
+	//메인 화면 *(회원관리 페이지) 뷰 요청
+	@RequestMapping("/main.mmy")
+	public ModelAndView mainView(ModelAndView mv , AdminVO avo ,PageUtil page) {
+		String view = "admin/main";
+		
+		mv.setViewName(view);
+		
+		return mv;
+	}
+	
+	
 	
 }
